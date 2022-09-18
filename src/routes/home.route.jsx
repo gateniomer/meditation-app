56389e7a6d358d677c0054abac3a26541f0768a8
Omer Gatenio <gateniomer@gmail.com>
2022-09-18 13:10:32 +0300

    added quote functionallity

--- src/routes/home.route.jsx
+++ src/routes/home.route.jsx
@@ -4,6 +4,7 @@ import { getExerciseListFromLocalStorage,saveExerciseListToLocalStorage } from '
 import { useState } from 'react';
 const Home = () => {
   const [history,setHistory] = useState(getExerciseListFromLocalStorage());
+  const [quote,setQuote] = useState('');
   const onDelHistory = (acc)=>{
   const historyList = getExerciseListFromLocalStorage();
   historyList.splice(acc,1);
@@ -14,9 +15,21 @@ const Home = () => {
     const newList = onDelHistory(acc);
     setHistory(newList);
   }
+
+  //fetch new quote
+  if(quote==='')
+  fetch('https://api.quotable.io/random?maxLength=70').then(response => response.json())
+  .then(data=>setQuote(data))
+  .catch(err => console.log(err))
+
   return(
     <div className="bg-gradient-to-r from-cyan-500 to-blue-500 w-full h-screen flex flex-col justify-center items-center">
-      <h1 className='text-7xl text-white font-bold mb-10'>Hello, @user 🧘</h1>
+      <h1 className='text-8xl text-white font-bold font-Lobster'>Hello, @user <span className='text-9xl'>🧘</span></h1>
+      <span className='text-3xl text-gray-700 max-w-xl font-Lobster mb-3 italic'>
+      {quote && quote.content}
+      <br/>
+      <span className='text-2xl text-gray-500 font-bold bg-slate-300 px-4 rounded-full not-italic'>{quote.author}</span>
+      </span>
       <SelectExerciseButton/>
       <ExerciseHistory history={history} onDelHistory={onDelHistoryHandler}/>
     </div>
