715755cac2bd8a221690859b9a8109588d919abc
gateniomer <gateniomer@gmail.com>
2022-09-18 11:07:03 +0300

    design changes

--- src/components/exerciseHistory/exerciseHistory.component.jsx
+++ src/components/exerciseHistory/exerciseHistory.component.jsx
@@ -1,17 +1,17 @@
 import { timeFormat } from "../../utils/utils";
 
 const ExerciseHistory = ({history,onDelHistory}) =>{
-  return (<div>
+  return (<div className="md:w-3/12">
     <h2 className="text-2xl font-bold my-2">History</h2>
     {history.map((item,acc) => {
     const time = timeFormat(item.time);
-    return <div key={acc} className='flex my-2 place-content-between'>
-      <span className="bg-slate-300 p-3 rounded-full mr-2">{acc+1}</span>
-      <div>
-      <h2>{item.exercise.title}</h2>
-      <h2>{time}</h2>
+    return <div key={acc} className='flex my-2 place-content-between bg-slate-50 rounded-full px-2 items-center shadow-lg border-blue-500 border-4'>
+      <span className="bg-slate-300 leading-8 text-center w-8 h-8 rounded-full">{acc+1}</span>
+      <div className="mx-3">
+      <span className="block font-bold">{item.exercise.title}</span>
+      <span className="text-gray-600">{time}</span>
       </div>
-      <button className="bg-slate-400 p-3 rounded-full" onClick={()=>onDelHistory(acc)}>x</button>
+      <button className="w-8 h-8 bg-slate-400 rounded-full" onClick={()=>onDelHistory(acc)}>x</button>
       </div>
 })}
   </div>)
--- src/routes/home.route.jsx
+++ src/routes/home.route.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   }
   return(
     <div className="bg-gradient-to-r from-cyan-500 to-blue-500 w-full h-screen flex flex-col justify-center items-center">
-      <h1 className='text-7xl text-white font-bold mb-10'>Hello, Omer.</h1>
+      <h1 className='text-7xl text-white font-bold mb-10'>Hello, @user 🧘</h1>
       <SelectExerciseButton/>
       <ExerciseHistory history={history} onDelHistory={onDelHistoryHandler}/>
     </div>
