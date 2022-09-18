16342d8cf3122103fff3a98424c3379db1112671
Omer Gatenio <gateniomer@gmail.com>
2022-09-18 13:38:47 +0300

    design changes

--- src/routes/home.route.jsx
+++ src/routes/home.route.jsx
@@ -2,9 +2,11 @@ import SelectExerciseButton from '../components/selectExerciseButton/selectExerc
 import ExerciseHistory from '../components/exerciseHistory/exerciseHistory.component';
 import { getExerciseListFromLocalStorage,saveExerciseListToLocalStorage } from '../utils/utils';
 import { useState } from 'react';
+import EXERCISES from '../exercises';
 const Home = () => {
   const [history,setHistory] = useState(getExerciseListFromLocalStorage());
   const [quote,setQuote] = useState('');
+  const [exerciseSelected,setExericeSelected] = useState(EXERCISES.MEDITATE);
   const onDelHistory = (acc)=>{
   const historyList = getExerciseListFromLocalStorage();
   historyList.splice(acc,1);
@@ -23,14 +25,33 @@ const Home = () => {
   .catch(err => console.log(err))
 
   return(
-    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 w-full h-screen flex flex-col justify-center items-center">
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 w-full h-screen flex justify-center items-center px-10">
+      <div>
       <h1 className='text-8xl text-white font-bold font-Lobster'>Hello, @user <span className='text-9xl'>🧘</span></h1>
       <span className='text-3xl text-gray-700 max-w-xl font-Lobster mb-3 italic'>
       {quote && quote.content}
       <br/>
       <span className='text-2xl text-gray-500 font-bold bg-slate-300 px-4 rounded-full not-italic'>{quote.author}</span>
       </span>
-      <SelectExerciseButton/>
+
+      <div className='flex items-center w-4/12 my-2'>
+      {Object.keys(EXERCISES).map(key => 
+        <span 
+        key={key} 
+        className={`bg-slate-200 rounded-full px-3 py-0.5 shadow-sm cursor-pointer ml-3 ${exerciseSelected.title === EXERCISES[key].title ? 'border-red-400 border-4 animate-wiggle' : ''}`}
+        onClick={()=>{
+          setExericeSelected(EXERCISES[key]);
+          // setSelectMode(false);
+          }}>
+        {EXERCISES[key].title}
+        </span>
+        )}
+      </div>
+      <span className='text-3xl font-bold text-cyan-300'>{exerciseSelected.title}</span>
+      <br/>
+      <span className='text-1xl max-w-xs'>{exerciseSelected.shortDescription}</span>
+      </div>
+      {/* <SelectExerciseButton/> */}
       <ExerciseHistory history={history} onDelHistory={onDelHistoryHandler}/>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,16 @@ module.exports = {
       'app':['Roboto','sans-serif'],
       'Lobster':['Lobster,sans-serif']
     },
+    animation: {
+      wiggle: 'wiggle 1s ease-in-out infinite',
+    },
     extend: {
       keyframes: {
         wiggle: {
           '0%, 100%': { transform: 'rotate(-3deg)' },
-          '50%': { transform: 'rotate(3deg)' },
-        }
+          '50%': { transform: 'rotate(3deg)',transform:'scale(1.2)' },
+        },
+        
       }
     },
   },
