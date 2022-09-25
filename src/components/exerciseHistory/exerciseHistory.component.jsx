326ba38b7e612d63215d8e3d11e2abfa5a1869fe
Omer Gatenio <gateniomer@gmail.com>
2022-09-26 02:12:11 +0300

    design changes

--- src/components/exerciseHistory/exerciseHistory.component.jsx
+++ src/components/exerciseHistory/exerciseHistory.component.jsx
@@ -12,17 +12,16 @@ const ExerciseHistory = ({list}) =>{
 
   return (history.length ? <div className="max-w-[400px] w-full">
     <h2 className="text-4xl text-white font-Lobster font-bold my-2">Exercises History</h2>
-    <div className="overflow-scroll overflow-x-hidden max-h-[200px] scrollbar">
+    <div className="overflow-scroll overflow-x-hidden max-h-[200px] scrollbar px-2">
     {history.map((item,acc) => {
     const time = timeFormat(item.time);
-    return <div key={acc} className='flex my-2 place-content-between bg-slate-50 rounded-full px-2 items-center shadow-lg border-blue-500 border-4'>
-      <span className="bg-slate-300 leading-8 text-center w-8 h-8 rounded-full">{item.exercise.emoji}</span>
-      <div className="mx-3">
-      <span className="block text-gray-400 text-sm">{item.date}</span>
-      <span className="block font-bold">{item.exercise.title}</span>
-      <span className="text-gray-600">{time}</span>
+    return <div key={acc} className='flex my-2 place-content-between bg-mainColor rounded-xl p-2 items-center shadow-lg text-text-light'>
+      <span className="bg-blue-300 leading-[48px] text-center w-[48px] h-[48px] rounded-xl text-[24px]">{item.exercise.emoji}</span>
+      <div>
+      <span className="block font-bold">{item.exercise.title} for {time}</span>
+      <span className="block text-gray-300 text-sm">{item.date}</span>
       </div>
-      <button className="w-8 h-8 bg-slate-400 rounded-full" onClick={()=>onDelHistory(acc)}>x</button>
+      <button className="w-8 h-8 bg-blue-300 rounded-full text-xl" onClick={()=>onDelHistory(acc)}>x</button>
       </div>
 })}
     </div>
