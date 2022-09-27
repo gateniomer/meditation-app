f5a362cb0efef4e5cc725b8fc0c5828c6aff94d0
gateniomer <gateniomer@gmail.com>
2022-09-27 12:32:01 +0300

    redesign summary popup

--- src/components/summaryPopup/summaryPopup.component.jsx
+++ src/components/summaryPopup/summaryPopup.component.jsx
@@ -22,18 +22,14 @@ const SummaryPopup = ({state}) => {
 
   return (
   <div className="bg-black bg-opacity-60 text-text-light absolute w-full h-screen flex flex-col items-center justify-center">
-    <div className='bg-blue-600 shadow-xl p-5 rounded-lg animate-growOnce mx-5 max-w-[450px]'>
-      <h1 className='text-4xl font-Lobster'>Exercise Summary</h1>
-      <span className='text-lg font-Lobster font-light'>Way to go 👏</span>
-      <br />
-      <div className='flex lg:mx-5 my-5 gap-1'>
-        <div className='w-[30%] text-center overflow-hidden'>
-          <span className='text-5xl bg-purple-400 rounded-full p-2 leading-loose border-4 border-blue-400'>{state.exercise.emoji}</span>
-        </div>
-        <div className='w-[60%] flex'>
-          <span className='text-xl my-auto'>You finished <span className='font-bold font-Lobster'>{state.exercise.title}</span> exercise  for {time}!</span>
-        </div>
-      </div>
+    <div className='bg-bg-main-light shadow-xl p-5 rounded-lg animate-growOnce mx-5 max-w-[450px]'>
+      <h1 className='text-4xl font-Lobster'>Exercise Summary 🎉</h1>
+      <hr className='block mb-5 mt-2'/>
+      <p className='text-lg'>You successfully completed</p>
+      <span className='text-7xl font-bold font-Lobster inline-block mx-2'>{state.exercise.title}</span>
+      <span className='inline-block ml-2 text-xl'>for {time}</span>
+      <span className='block my-5 text-lg'>Way to go! {state.exercise.emoji}</span>
+      
       
       <div className='flex items-center place-content-around'>
       <Button onClickHandler={onDiscardHandler} className='w-[45%] bg-red-400 font-normal'>Discard</Button>
